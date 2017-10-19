15034370193a5d5c5bce0bad2ebd062ecb304ebb
Igor Sudenko <tommy.star@yandex.ru>
2017-10-19 18:38:11 +0300

    Добавил файл с трансляцией ютуба

---
+++ 1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>YOUTUBE STREAM</title>
+</head>
+<body>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/Ed0vEPI1YOE?rel=0" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
